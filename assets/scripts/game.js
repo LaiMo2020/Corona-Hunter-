6ddd6984433a0558f6b13d3c53eec1325d0cb5b9
LaiMo2020 <laithmohammad11@gmail.com>
2020-09-07 13:24:17 +0000

    README, updating somestyle, adding comments

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,7 +1,4 @@
-
-
-
-// creating the cols and rwos of the board
+// creating the cols and rwos of the board and declare them
 
 const $board = $("#board");
 const ROWS = 10;
@@ -16,7 +13,8 @@ function createBoard(rows, cols) {
         .addClass("col hidden") // adding hidding class
         .attr("data-row", i)
         .attr("data-col", j);
-      if (Math.random() < 0.1) { // adding the virus cell with 10% of the cell
+      if (Math.random() < 0.1) {
+        // adding the virus cell with 10% of the cell
         $col.addClass("virus");
       }
       $row.append($col);
@@ -25,6 +23,7 @@ function createBoard(rows, cols) {
   }
 }
 
+// restart the game after losing or winning
 function restart() {
   createBoard(ROWS, COLS);
 }
@@ -33,65 +32,56 @@ function gameOver(isWin) {
   let message = null;
   let icon = null;
   if (isWin) {
-    message = "YOU FOUND THE VIURSES!";
+    // message = "YOU FOUND THE VIURSES!"; if no virus cell's is clicked
     icon = "fa fa-flag";
   } else {
-    message = "THE VIRUS GOT YOU!";
+    message = "THE VIRUS GOT YOU!"; //  if the virus cell was clicked
     icon = "fa fa-cog";
   }
-  $(".col.virus").append(
-    $("<i>").addClass(icon)
-  );
-  $(".col:not(.virus)")
-    .html(function() {
-      const $cell = $(this);
-      const count = getVirusCount(
-        $cell.data("row"),
-        $cell.data("col"),
-      );
-      return count === 0 ? " " : count;
-    })
-  $('.col.hidden').removeClass("hidden");
-  setTimeout(function() {
+  $(".col.virus").append($("<i>").addClass(icon));
+  $(".col:not(.virus)").html(function () {
+    const $cell = $(this);
+    const count = getVirusCount($cell.data("row"), $cell.data("col"));
+     return count === 0 ? " " : count; // hiding the number od the cell has no viruses after the game over 
+  });
+  $(".col.hidden").removeClass("hidden");
+  setTimeout(function () {
     alert(message);
     restart();
   }, 1000);
 }
-
+//  tracking where are recrusions is going  / json locations/
 function reveal(oi, oj) {
   const seen = {};
 
   function helper(i, j) {
     if (i >= ROWS || j >= COLS || i < 0 || j < 0) return;
-    const key = `${i} ${j}`
+    const key = `${i} ${j}`;
     if (seen[key]) return;
-    const $cell =
-      $(`.col.hidden[data-row=${i}][data-col=${j}]`);
+    const $cell = $(`.col.hidden[data-row=${i}][data-col=${j}]`);
     const virusCount = getVirusCount(i, j);
-    if (
-      !$cell.hasClass("hidden") ||
-      $cell.hasClass("virus")
-    ) {
+    if (!$cell.hasClass("hidden") || $cell.hasClass("virus")) {
       return;
     }
 
-    $cell.removeClass("hidden");
+    $cell.removeClass("hidden"); // empty opened cell
 
+    // stop it to be reversing further after clicking on cell that has a number on it
     if (virusCount) {
       $cell.text(virusCount);
       return;
     }
-    
+
     for (let di = -1; di <= 1; di++) {
       for (let dj = -1; dj <= 1; dj++) {
         helper(i + di, j + dj);
-      }      
+      }
     }
   }
 
   helper(oi, oj);
 }
-
+// numbers of viruses around the clicked cell 
 function getVirusCount(i, j) {
   let count = 0;
   for (let di = -1; di <= 1; di++) {
@@ -99,26 +89,25 @@ function getVirusCount(i, j) {
       const ni = i + di;
       const nj = j + dj;
       if (ni >= ROWS || nj >= COLS || nj < 0 || ni < 0) continue;
-      const $cell =
-        $(`.col.hidden[data-row=${ni}][data-col=${nj}]`);
-      if ($cell.hasClass('virus')) count++;
-    }      
+      const $cell = $(`.col.hidden[data-row=${ni}][data-col=${nj}]`);
+      if ($cell.hasClass("virus")) count++;
+    }
   }
   return count;
 }
 // adding a click listener
-$board.on("click", ".col.hidden", function() {
+$board.on("click", ".col.hidden", function () {
   const $cell = $(this);
   const row = $cell.data("row");
   const col = $cell.data("col");
-  
+  //   if the a cell with a virus is clicked
   if ($cell.hasClass("virus")) {
     gameOver(false);
   } else {
     reveal(row, col);
-    const isGameOver = $(".col.hidden").length === $('.col.virus').length
+    const isGameOver = $(".col.hidden").length === $(".col.virus").length;
     if (isGameOver) gameOver(true);
   }
-})
+});
 
 restart();
