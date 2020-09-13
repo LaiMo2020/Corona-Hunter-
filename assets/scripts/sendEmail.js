b69387d4e5316aa026175000f17878f8c189c73f
LaiMo2020 <laithmohammad11@gmail.com>
2020-09-13 17:40:45 +0000

    smaller button in smaller devices

--- assets/scripts/sendEmail.js
+++ assets/scripts/sendEmail.js
@@ -1,17 +1,27 @@
 
+
 var templateParams = {
-    name: 'laith',
-    email: 'Check this out!',
-    value: "send"
+   name: 'laith',
+   email: 'Check this out!',
+   value: "send"
 };
- 
-emailjs.send('gmail', 'laith', {
-        "from_name": contactForm.name,
-        "from_email": contactForm.emailaddress,
-        "project_request": contactForm.projectsummary
-    })
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-       console.log('FAILED...', error);
-    });
+
+let form = document.getElementById('contactForm');
+let conf = document.getElementById('conf');
+
+window.onload = () => {
+   form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      emailjs.send('gmail', 'laith', {
+         "from_name": contactForm.name,
+         "from_email": contactForm.emailaddress,
+         "project_request": contactForm.projectsummary
+      })
+         .then(function (response) {
+            console.log('SUCCESS!', response.status, response.text);
+            conf.style.opacity = 1;
+         }, function (error) {
+            console.log('FAILED...', error);
+         });
+   });
+}
